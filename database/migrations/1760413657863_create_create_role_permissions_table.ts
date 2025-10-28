4ad62876bbe5514cef14e5cf1e4cc619e7436823
Admin <admin@example.com>
2025-10-28 16:59:28 +0700

    update

--- database/migrations/1760413657863_create_create_role_permissions_table.ts
+++ database/migrations/1760413657863_create_create_role_permissions_table.ts
@@ -4,21 +4,23 @@ export default class extends BaseSchema {
   protected tableName = 'role_permissions'
 
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.integer('role_id').unsigned().references('id').inTable('roles').onDelete('CASCADE')
-      table
-        .integer('permission_id')
-        .unsigned() // angka tanpa tanda negatif
-        // biar tipe data-nya sama dengan kolom roles.id
-        .references('id')
-        .inTable('permissions')
-        .onDelete('CASCADE')
+    if (!(await this.schema.hasTable(this.tableName))) {
+      this.schema.createTable(this.tableName, (table) => {
+        table.integer('role_id').unsigned().references('id').inTable('roles').onDelete('CASCADE')
+        table
+          .integer('permission_id')
+          .unsigned() // angka tanpa tanda negatif
+          // biar tipe data-nya sama dengan kolom roles.id
+          .references('id')
+          .inTable('permissions')
+          .onDelete('CASCADE')
 
-      // Mencegah duplikasi entri (satu role tidak bisa punya permission yang sama dua kali)
-      table.primary(['role_id', 'permission_id'])
+        // Mencegah duplikasi entri (satu role tidak bisa punya permission yang sama dua kali)
+        table.primary(['role_id', 'permission_id'])
 
-      table.timestamp('created_at', { useTz: true })
-    })
+        table.timestamp('created_at', { useTz: true })
+      })
+    }
   }
 
   async down() {
